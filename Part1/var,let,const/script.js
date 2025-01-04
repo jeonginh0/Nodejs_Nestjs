4dde85201b82e3452e7d85f43a8826e147a4a25f
jeonginh0 <stitch010913@gmail.com>
2025-01-04 23:42:40 +0900

    250104 study

---
+++ Part1/var,let,const/script.js
@@ -0,0 +1,25 @@
+// var greeting = 'hello';
+// console.log(greeting);
+
+// var greeting = 'hi';
+// console.log(greeting);
+
+// greeting = 'how are you?'
+// console.log(greeting);
+
+// let : 중복 선언 불가, 재할당 가능
+// let greeting = 'hello';
+// console.log(greeting);
+
+// let greeting = 'hi';
+// console.log(greeting);
+
+// greeting = 'hi';
+// console.log(greeting);
+
+// const : 중복 선언 X, 재할당 X
+const greeting = 'hello';
+console.log(greeting);
+
+greeting = 'hi';
+console.log(greeting);
